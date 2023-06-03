57d6a06f4bf9639d12e0c9390ef86da3399b997f
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-03 12:37:58 -0400

    chore: adding git hooks and formatting

--- infra/versions.tf
+++ infra/versions.tf
@@ -14,7 +14,7 @@ terraform {
       version = ">= 4.67.0, != 4.31.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">= 3.5.1"
     }
   }
