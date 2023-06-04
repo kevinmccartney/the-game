c5971c52950443716d7ed5bdcbda1affebbb97b9
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-04 17:04:13 -0400

    feat(client): starting work on homepage

---
+++ the-game-client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Navbar = () => (
+  <Box>
+    <FontAwesomeIcon icon="dice" />
+    Foo
+  </Box>
+);
