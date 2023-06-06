98a4b74d9144b04263c99175b3aea1448175c2a6
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-06 11:24:43 -0400

    feat(client): adding landing and login pages

---
+++ the-game-client/src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Roboto', Arial, Helvetica, sans-serif`,
+    body: `'Roboto', Arial, Helvetica, sans-serif`,
+  },
+});
+
+export default theme;
