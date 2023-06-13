e25ab0c7e512127094d03b03fa70cc81763cd1df
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-12 20:26:15 -0400

    fix(infra): fixing error paage for web host bucket

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,7 @@ resource "google_storage_bucket" "web_client" {
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+    not_found_page   = "index.html"
   }
 
   versioning {
