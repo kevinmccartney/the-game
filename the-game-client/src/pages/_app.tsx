5469a95b9088e390af6ede2d8d41fd3b5b94fded
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-20 00:03:45 -0400

    fix(client): remove index.html from URL

--- the-game-client/src/pages/_app.tsx
+++ the-game-client/src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import './globals.css';
 
+// https://issuetracker.google.com/issues/151713401#comment3
+// https://react.dev/learn/synchronizing-with-effects#not-an-effect-initializing-the-application
+const url = window.location.href;
+if (url.includes('index.html')) {
+  window.location.href = url.replace('index.html', '');
+}
+
 export default function MyApp({
   Component,
   pageProps,
