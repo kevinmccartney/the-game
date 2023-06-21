cb54377489187e4d12a648084d2e80d8df56a37c
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-21 12:23:02 -0400

    feat(client): routing back to home on auth change, using auth data on dash

--- the-game-client/src/pages/index.tsx
+++ the-game-client/src/pages/index.tsx
@@ -2,20 +2,12 @@
 
 import React from 'react';
 
-import { Heading, VStack } from '@chakra-ui/react';
-import { DefaultLayout } from '@the-game/client/components/layouts';
 import { getAuth } from 'firebase/auth';
 import Dashboard from '@the-game/client/components/dashboard';
 import Splash from '@the-game/client/components/splash/Splash';
-// import { useRouter } from 'next/router';
-// import { getAuth } from 'firebase/auth';
 
 const AppRoot = () => {
   const auth = getAuth();
-  // const router = useRouter();
-  // if (auth.currentUser) {
-  //   router.replace('/dashboard');
-  // }
 
   return auth.currentUser ? <Dashboard /> : <Splash />;
 };
