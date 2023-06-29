8996e8b6ccec5ac77d4dc0322311664505757d11
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-28 22:56:28 -0400

    fix(infra): switch firestore to native mode

--- infra/database.tf
+++ infra/database.tf
@@ -2,7 +2,7 @@ resource "google_firestore_database" "the_game_firestore" {
   project                     = var.project_id
   name                        = "(default)"
   location_id                 = "nam5"
-  type                        = "DATASTORE_MODE"
+  type                        = "FIRESTORE_NATIVE"
   concurrency_mode            = "OPTIMISTIC"
   app_engine_integration_mode = "DISABLED"
 }
