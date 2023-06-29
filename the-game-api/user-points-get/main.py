88e754ca7161daa8178412690eec26a55345323e
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-29 19:35:59 -0400

    feat(api): user-points-get business logic

--- the-game-api/user-points-get/main.py
+++ the-game-api/user-points-get/main.py
@@ -1,8 +1,32 @@
+import traceback
+import re
+
 import functions_framework
+from flask import Request
+import firebase_admin
+from firebase_admin import firestore, auth
+from google.cloud.logging import Client as LoggingClient
+from google.cloud.firestore import Client as FirestoreClient
+from firebase_admin.auth import (
+    InvalidIdTokenError,
+    ExpiredIdTokenError,
+    RevokedIdTokenError,
+    UserDisabledError,
+)
+from google.cloud.firestore_v1.base_query import FieldFilter
+
+logging_client = LoggingClient()
+logging_client.setup_logging()
+
+import logging
+
+firebase_admin.initialize_app()
+
+db: FirestoreClient = firestore.client()
 
 
 @functions_framework.http
-def function_handler(request):
+def function_handler(request: Request):
     """HTTP ping Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -10,4 +34,46 @@ def function_handler(request):
     Returns:
         Pong.
     """
-    return "Pong"
+    try:
+        id_token = request.headers.get("x_forwarded_authorization").replace(
+            "Bearer ", ""
+        )
+
+        auth.verify_id_token(id_token=id_token)
+    except AttributeError:
+        logging.error(traceback.format_exc())
+        return {"code": 401, "message": "Unauthorized: Authorization not provided"}, 401
+    except (
+        InvalidIdTokenError,
+        ExpiredIdTokenError,
+        RevokedIdTokenError,
+        UserDisabledError,
+    ) as ex:
+        print(ex)
+        logging.error(traceback.format_exc())
+        return {
+            "code": 403,
+            "message": "Forbidden: Caller is not authorized to take this action",
+        }, 403
+    except Exception:
+        logging.error(traceback.format_exc())
+        return {"code": 500, "message": "Internal server error"}, 500
+
+    try:
+        pattern = re.compile(r"^\/v1\/users\/(.*)\/points$")
+        request_path = request.headers.get("x-envoy-original-path")
+        subject_uid = pattern.sub(r"\1", request_path)
+
+        points_ref = db.collection("points")
+        query_ref = points_ref.where(
+            filter=FieldFilter(field_path="subject", op_string="==", value=subject_uid)
+        )
+
+        results = query_ref.get()
+
+        points = [doc.to_dict() for doc in results]
+
+        return points, 200
+    except Exception:
+        logging.error(traceback.format_exc())
+        return {"code": 500, "message": "Internal server error"}, 500
