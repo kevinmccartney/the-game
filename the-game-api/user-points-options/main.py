8994cacc35bb4ceb18804d685137c669eb3c0af4
Kevin McCartney <kevin.mccartney@insight.com>
2023-06-29 22:20:01 -0400

    feat(api): deploying user-points-options

---
+++ the-game-api/user-points-options/main.py
@@ -0,0 +1,21 @@
+import functions_framework
+from flask import Request
+
+
+@functions_framework.http
+def function_handler(request: Request):
+    """HTTP ping Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        Pong.
+    """
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600",
+    }
+
+    return ("", 204, headers)
