d9f9f01484f4e62771aca80aa3a5c434ea4d5df2
Kevin McCartney <kevin.mccartney@insight.com>
2023-07-02 14:46:20 -0400

    feat(api): users endpoints

---
+++ the-game-api/users-get-options/main.py
@@ -0,0 +1,21 @@
+import functions_framework
+from flask import Request
+
+
+@functions_framework.http
+def function_handler(request: Request):
+    """HTTP ping Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        CORS headers.
+    """
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Max-Age": "3600",
+    }
+
+    return ("", 204, headers)
