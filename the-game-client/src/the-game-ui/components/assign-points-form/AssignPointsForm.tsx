e4971025d199696419f598807253aab724e48f00
Kevin McCartney <kevin.mccartney@insight.com>
2023-07-04 15:00:19 -0400

    feat(client): posting point and error handling

--- the-game-client/src/the-game-ui/components/assign-points-form/AssignPointsForm.tsx
+++ the-game-client/src/the-game-ui/components/assign-points-form/AssignPointsForm.tsx
@@ -1,20 +1,31 @@
 import {
-  Box,
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
-  NumberInput,
-  NumberInputField,
+  useToast,
 } from '@chakra-ui/react';
 
 import { UserSearchField } from '@the-game/client/the-game-ui/components';
 import { AssignPointsForm as AssignPointsFormModel } from '@the-game/client/the-game-ui/models';
+import { getAuth } from 'firebase/auth';
+import { noop } from 'lodash-es';
 import { useEffect, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { SubmitHandler, UseFormReturn } from 'react-hook-form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faS, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-export const AssignPointsForm = (props: any) => {
+export const AssignPointsForm = ({
+  showCancel,
+  onClose,
+  form,
+}: {
+  showCancel?: boolean;
+  onClose?: () => void;
+  form: UseFormReturn<AssignPointsFormModel, any, undefined>;
+}) => {
   const {
     register,
     handleSubmit,
@@ -22,18 +33,18 @@ export const AssignPointsForm = (props: any) => {
     formState: { errors },
     watch,
     getValues,
-  } = useForm<AssignPointsFormModel>();
-  const onSubmit: SubmitHandler<AssignPointsFormModel> = (data) => {
-    console.log(errors);
-    console.log(data);
+  } = form;
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const auth = getAuth();
+  const onSubmit: SubmitHandler<AssignPointsFormModel> = () => {
+    setIsLoading(true);
+    postPoint(toast);
   };
   const [values, setValues] = useState(getValues());
 
   useEffect(() => {
-    const subscription = watch((value, { name, type }) => {
-      // console.log(errors);
-      // console.log(value, name, type);
-
+    const subscription = watch(() => {
       const values = getValues();
 
       setValues(values);
@@ -42,72 +53,140 @@ export const AssignPointsForm = (props: any) => {
     return () => subscription.unsubscribe();
   });
 
+  const postPoint = async (toast: any) => {
+    const token = await auth.currentUser?.getIdToken();
+
+    await fetch(
+      `https://api.the-game.kevinmccartney.dev/v1/users/${values.subject}/points`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          reason: values.reason,
+          // value from number field comes through as a string
+          points: parseInt(values.points as unknown as string),
+        }),
+      },
+    );
+
+    setIsLoading(false);
+
+    if (onClose) {
+      onClose();
+    }
+
+    toast({
+      title: 'Point Created.',
+      // description: "Point cre.",
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+      variant: 'top-accent',
+    });
+  };
+
   return (
-    <Box mt={8}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Flex
-          gap={8}
-          flexDirection="column"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Flex
+        gap={8}
+        mt={{ base: 8, md: 0 }}
+        flexDirection={{ base: 'column', md: 'row' }}
+        flexWrap={{ md: 'wrap' }}
+      >
+        <UserSearchField
+          register={register}
+          setValue={setValue}
+          errors={errors}
+          containerProps={{ width: { md: '75%' } }}
+          form={form}
+        />
+        <FormControl
+          width={{ md: '45%' }}
+          isInvalid={!!errors.reason}
         >
-          <UserSearchField
-            register={register}
-            setValue={setValue}
-            errors={errors}
+          <FormLabel>Reason</FormLabel>
+          <Input
+            {...register('reason', { required: 'Required' })}
+            placeholder="So good at making websites 🤗"
+            _placeholder={{
+              color: errors.reason ? 'red.500' : 'gray.400',
+            }}
+            borderColor={errors.reason ? 'red.500' : 'gray.200'}
+            className={errors.reason ? 'hover:border-red-500' : ''}
+            colorScheme="red"
           />
-          <FormControl>
-            <FormLabel>Reason</FormLabel>
-            <Input
-              {...register('reason', { required: true })}
-              placeholder="So good at making websites 🤗"
-              _placeholder={{
-                color: errors.reason
-                  ? 'red.500'
-                  : values?.reason?.length > 0
-                  ? 'gray.800'
-                  : 'gray.400',
-              }}
-              borderColor={errors.reason ? 'red.500' : 'gray.200'}
-              className={errors.reason ? 'hover:border-red-500' : ''}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Points</FormLabel>
-            <NumberInput>
-              <NumberInputField
-                placeholder="50"
-                _placeholder={{
-                  color: errors.points
-                    ? 'red.500'
-                    : values?.points
-                    ? 'gray.800'
-                    : 'gray.400',
-                }}
-                borderColor={errors.points ? 'red.500' : 'gray.200'}
-                className={errors.points ? 'hover:border-red-500' : ''}
-                {...register('points', { required: true })}
-              />
-            </NumberInput>
-          </FormControl>
-        </Flex>
-        <Flex
-          mt={8}
-          justifyContent="flex-end"
+          {errors.reason && (
+            <FormErrorMessage>{errors.reason.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl
+          width={{ md: '45%' }}
+          isInvalid={!!errors.points}
         >
+          <FormLabel>Points</FormLabel>
+          {/* <NumberInput> */}
+          <Input
+            placeholder="50"
+            _placeholder={{
+              color: errors.points ? 'red.500' : 'gray.400',
+            }}
+            borderColor={errors.points ? 'red.500' : 'gray.200'}
+            className={errors.points ? 'hover:border-red-500' : ''}
+            {...register('points', {
+              required: 'Required',
+              validate: (value) => {
+                return (
+                  !isNaN(parseInt(value as any)) ||
+                  'Must be a positive or negative number'
+                );
+              },
+            })}
+            inputMode="numeric"
+          />
+          {/* </NumberInput> */}
+          {errors.points && (
+            <FormErrorMessage>{errors.points.message}</FormErrorMessage>
+          )}
+        </FormControl>
+      </Flex>
+      <Flex
+        mt={8}
+        justifyContent="flex-end"
+      >
+        {showCancel && (
           <Button
             variant="outline"
             mr={3}
-            onClick={props.onClose}
+            onClick={onClose || noop}
           >
             Cancel
           </Button>
-          <Button
-            type="submit"
-            colorScheme="blue"
-          >
-            Submit
-          </Button>
-        </Flex>
-      </form>
-    </Box>
+        )}
+
+        <Button
+          type="submit"
+          colorScheme={isLoading ? 'gray' : 'blue'}
+          disabled={isLoading}
+          leftIcon={
+            isLoading ? (
+              <FontAwesomeIcon
+                icon={faSpinner}
+                spin
+              />
+            ) : undefined
+          }
+        >
+          {isLoading ? 'Loading...' : 'Submit'}
+        </Button>
+      </Flex>
+    </form>
   );
+};
+
+AssignPointsForm.defaultProps = {
+  showCancel: false,
+  onClose: () => {},
+  inverse: false,
 };
--- the-game-client/src/the-game-ui/components/user-search-field/UserSearchField.tsx
+++ the-game-client/src/the-game-ui/components/user-search-field/UserSearchField.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
@@ -16,12 +17,17 @@ import {
   useOutsideClick,
 } from '@chakra-ui/react';
 import { AssignPointsForm, User } from '@the-game/client/the-game-ui/models';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { debounce } from 'lodash-es';
 import { getAuth } from 'firebase/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FieldErrors, UseFormRegister, UseFormSetValue } from 'react-hook-form';
+import {
+  FieldErrors,
+  UseFormRegister,
+  UseFormReturn,
+  UseFormSetValue,
+} from 'react-hook-form';
 
 const getUsers = async (
   search: string,
@@ -52,10 +58,14 @@ export const UserSearchField = ({
   register,
   setValue,
   errors,
+  containerProps,
+  form,
 }: {
   register: UseFormRegister<AssignPointsForm>;
   setValue: UseFormSetValue<AssignPointsForm>;
   errors: FieldErrors<AssignPointsForm>;
+  containerProps?: any;
+  form: UseFormReturn<AssignPointsForm, any, undefined>;
 }) => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -83,6 +93,7 @@ export const UserSearchField = ({
   const handleSelect = (user: User) => {
     setSelectedUser(user);
     setValue('subject', user.uid);
+    form.trigger('subject');
 
     setSearchTerm(user?.display_name ?? '');
     setSuggestionsOpen(false);
@@ -117,111 +128,109 @@ export const UserSearchField = ({
     getUsers('', auth, setUsers, setIsLoading);
   };
 
-  // const registeredProps = useCallback(
-  //   () => register('subject', { required: true }),
-  //   [register],
-  // );
-
   return (
-    <Box ref={fieldRef}>
-      <FormControl>
-        <FormLabel>User</FormLabel>
-        <InputGroup>
-          {selectedUser && (
-            <InputLeftElement
-              pointerEvents="none"
-              top={1}
-              left={1}
+    <FormControl
+      ref={fieldRef}
+      isInvalid={!!errors.subject}
+      {...containerProps}
+    >
+      <FormLabel>User</FormLabel>
+      <InputGroup>
+        {selectedUser && (
+          <InputLeftElement
+            pointerEvents="none"
+            top={1}
+            left={1}
+          >
+            <Avatar
+              size="sm"
+              name={selectedUser.display_name}
+              src={selectedUser.photo_url || ''}
+              referrerPolicy="no-referrer"
+            />
+          </InputLeftElement>
+        )}
+        <Input
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+            debouncedSearch(event.target.value, auth, setUsers, setIsLoading);
+          }}
+          placeholder="Search for a friend..."
+          _placeholder={{
+            color: errors.subject?.type === 'required' ? 'red.500' : 'gray.400',
+          }}
+          onFocus={handleFocus}
+          onKeyDown={handleKeydown}
+          ref={inputRef}
+          value={searchTerm}
+          backgroundColor={selectedUser ? 'green.100' : 'initial'}
+          borderColor={errors.subject ? 'red.500' : 'gray.200'}
+          className={errors.subject ? 'hover:border-red-500' : ''}
+          // p={6}
+          size="lg"
+          pl={selectedUser ? 12 : 6}
+        />
+        <Input
+          {...register('subject', { required: 'Required' })}
+          display="none"
+        />
+        {selectedUser && (
+          <InputRightElement
+            top={1}
+            right={2}
+          >
+            <Button
+              colorScheme="green"
+              variant="ghost"
+              onClick={clearSelectedUser}
             >
-              <Avatar
-                size="sm"
-                name={selectedUser.display_name}
-                src={selectedUser.photo_url || ''}
-                referrerPolicy="no-referrer"
-              />
-            </InputLeftElement>
-          )}
-          <Input
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-              debouncedSearch(event.target.value, auth, setUsers, setIsLoading);
-            }}
-            placeholder="Search for a friend..."
-            _placeholder={{
-              color:
-                errors.subject?.type === 'required' ? 'red.500' : 'gray.800',
-            }}
-            onFocus={handleFocus}
-            onKeyDown={handleKeydown}
-            ref={inputRef}
-            value={searchTerm}
-            backgroundColor={selectedUser ? 'green.100' : 'initial'}
-            borderColor={errors.subject ? 'red.500' : 'gray.200'}
-            className={errors.subject ? 'hover:border-red-500' : ''}
-            p={6}
-            pl={selectedUser ? 12 : 6}
-          />
-          <Input
-            // value={selectedUserUID}
-            {...register('subject', { required: true })}
-            display="none"
-          />
-          {selectedUser && (
-            <InputRightElement
-              top={1}
-              right={2}
-            >
-              <Button
-                colorScheme="green"
-                variant="ghost"
-                onClick={clearSelectedUser}
-              >
-                <FontAwesomeIcon icon={faXmark} />
-              </Button>
-            </InputRightElement>
-          )}
-        </InputGroup>
-        <Menu
-          isOpen={suggestionsOpen}
-          offset={[0, -12]}
-        >
-          <MenuButton></MenuButton>
-          {users.length > 0 && !isLoading && (
-            <MenuList rootProps={{ style: { width: '100%' } }}>
-              {users.map((result, index) => {
-                const user = result as unknown as User;
-                const Icon = () =>
-                  (user as any).icon ? (
-                    <Avatar icon={(user as any).icon} />
-                  ) : (
-                    <Avatar
-                      name={user.display_name}
-                      src={user.photo_url || ''}
-                      referrerPolicy="no-referrer"
-                    />
-                  );
-
-                return (
-                  <MenuItem
-                    icon={<Icon />}
-                    mt={index === 0 ? 4 : 0}
-                    borderBottomColor="gray.200"
-                    borderBottomWidth={index === users.length - 1 ? 0 : 1}
-                    key={user.uid}
-                    onClick={() => handleSelect(user)}
-                    isDisabled={!!(user as any).isDisabled}
-                  >
-                    {user.display_name}
-                  </MenuItem>
+              <FontAwesomeIcon icon={faXmark} />
+            </Button>
+          </InputRightElement>
+        )}
+      </InputGroup>
+      {errors.subject && (
+        <FormErrorMessage>{errors.subject.message}</FormErrorMessage>
+      )}
+      <Menu
+        isOpen={suggestionsOpen}
+        offset={[0, -12]}
+      >
+        <MenuButton></MenuButton>
+        {users.length > 0 && !isLoading && (
+          <MenuList rootProps={{ style: { width: '100%' } }}>
+            {users.map((result, index) => {
+              const user = result as unknown as User;
+              const Icon = () =>
+                (user as any).icon ? (
+                  <Avatar icon={(user as any).icon} />
+                ) : (
+                  <Avatar
+                    name={user.display_name}
+                    src={user.photo_url || ''}
+                    referrerPolicy="no-referrer"
+                  />
                 );
-              })}
-            </MenuList>
-          )}
-          {users.length == 0 && !isLoading && <Text>No results</Text>}
-          {isLoading && <Text>Loading...</Text>}
-        </Menu>
-        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
-      </FormControl>
-    </Box>
+
+              return (
+                <MenuItem
+                  icon={<Icon />}
+                  mt={index === 0 ? 4 : 0}
+                  borderBottomColor="gray.200"
+                  borderBottomWidth={index === users.length - 1 ? 0 : 1}
+                  key={user.uid}
+                  onClick={() => handleSelect(user)}
+                  isDisabled={!!(user as any).isDisabled}
+                >
+                  {user.display_name}
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        )}
+        {users.length == 0 && !isLoading && <Text>No results</Text>}
+        {isLoading && <Text>Loading...</Text>}
+      </Menu>
+    </FormControl>
   );
 };
