f830ccfe569ffb64dc678cb42d360281ed376ebc
Kevin McCartney <kevin.mccartney@insight.com>
2023-07-06 18:35:01 -0400

    chore(api): moving to projects dir

---
+++ projects/api/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="the_game_api",
+    version="0.1.0",
+    description="The API for The Game",
+    author="Kevin McCartney",
+    author_email="hello@kevinmccartney.is",
+    url="https://the-game.kevinmccartney.dev",
+    py_modules=find_packages(),
+)
