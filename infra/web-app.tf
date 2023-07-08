9ec9336bc66222041a7bc3f260ed440f30c48cfc
Kevin McCartney <kevin.mccartney@insight.com>
2023-07-08 19:02:38 -0400

    fix(infra): routing 404s to 404 page instead of index

--- infra/web-app.tf
+++ infra/web-app.tf
@@ -23,7 +23,7 @@ resource "google_storage_bucket" "web_client" {
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "index.html"
+    not_found_page   = "404/index.html"
   }
 
   versioning {
